b195793607f2198c066fd0f3a54bdcbbf034dc26
sp1rale <dimayaremchyk24@gmail.com>
2023-10-03 14:57:23 +0300

    Ex-3 task was done

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-input_string= input('Введіть рядок: ')
-if input_string == input_string[::-1]:
- print('Введений рядок с палідромом: ')
-else:
- print('Введенийрядок не є палідромом: ')
+text = input("Введіть текст: ")
+reserved_words = input("Введіть список зарезервованих слів (через пробіл): ").split()
+
+words = text.split()
+
+for i in range(len(words)):
+    if words[i].lower() in reserved_words:
+        words[i] = words[i].upper()
+result_text = ' '.join(words)
+
+print(result_text)
